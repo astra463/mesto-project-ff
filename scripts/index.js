210b864f2d5b419e20e6da56a9245191b1356e11
Sergei Shilov <ya.sergei240300@yandex.ru>
2023-12-24 00:41:40 +0300

    add script.js

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,27 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const cardList = document.querySelector('.places__list'); //get card location
+
+
+function deleteCard(event) {
+    const deleteButton = event.target;
+    const listItem = deleteButton.closest('.places__item');
+    listItem.remove();
+}
+
+function addCard(cardInfo) {
+    const cardTemplate = document.querySelector('#card-template').content; //import card template
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true); //clone card template
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardElement.querySelector('.card__title').textContent = cardInfo.name;
+    cardElement.querySelector('.card__image').src = cardInfo.link;
+    deleteButton.addEventListener('click', deleteCard);
+
+    return cardElement;
+}
+
+initialCards.forEach(function(card) {
+    cardList.append(addCard(card));
+})
